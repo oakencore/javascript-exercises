f1ca4e580ad8e0a8997c910a5590aa4bef3fdc9c
oakencore <105373906+oakencore@users.noreply.github.com>
2022-08-10 10:37:51 +0200

    Added tempconversion workings

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  c = (5/9) * (temp - 32)
+  return Number(c.toFixed(1))
+  // toFixed returns a string representation of numObj. Number() is needed to convert it to an int. 
 };
 
-const ctof = function() {
 
+
+const ctof = function (temp) {
+  f = temp * 9 / 5 + 32
+  return Number(f.toFixed(1))
 };
+console.log(ftoc(-100));
+console.log(ctof(-10));
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
